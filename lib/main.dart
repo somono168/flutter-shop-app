ec7ff05cfda3909d5da2e4458dc2792f68b114f9
somono168 <chek.somono1@gmail.com>
2022-05-13 10:17:10 +0700

    Add form create/edit product page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
 
 import './providers/cart.dart';
 import './providers/orders.dart';
@@ -39,6 +40,7 @@ class MyApp extends StatelessWidget {
           CartScreen.routeName: (ctx) => const CartScreen(),
           OrdersScreen.routeName: (ctx) => const OrdersScreen(),
           UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
         },
       ),
     );
---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+// StatefulWidget because this screen need to handle Form input
+class EditProductScreen extends StatefulWidget {
+  static const routeName = '/edit-product';
+  const EditProductScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EditProductScreen> createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Product'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
